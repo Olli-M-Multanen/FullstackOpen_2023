c515d3e7b981aec50f2174566d3ee94392881c3f
Olli-M-Multanen <olli.multanen@gmail.com>
2023-02-24 09:59:01 +0100

    unicafe 1.7 step2 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,13 +14,16 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const Statistics = ({ good, neutral, bad}) => {
+const Statistics = ({ good, neutral, bad, total, positive}) => {
   return (
     <div>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {positive / total}</p>
+      <p>positive {good / total * 100} %</p>
     </div>
   )
 }
@@ -29,18 +32,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [positive, setPositive] = useState(0)
+
 
   const handleGood = () => {
     console.log("Good")
     setGood(good + 1)
+    setTotal(total +1)
+    setPositive(positive + 1)
   }
   const handleNeutral = () => {
     console.log("Neutral")
     setNeutral(neutral + 1)
+    setTotal(total +1)
   }
   const handleBad = () => {
     console.log("Bad")
     setBad(bad + 1)
+    setTotal(total +1)
+    setPositive(positive - 1)
   }
 
   return (
@@ -49,7 +60,7 @@ const App = () => {
     <Button handleClick={handleGood} text="good" />
     <Button handleClick={handleNeutral} text="neutral" />
     <Button handleClick={handleBad} text="bad" />
-    <Statistics good={good} neutral={neutral} bad={bad} /> 
+    <Statistics good={good} neutral={neutral} bad={bad} total={total} positive={positive}/> 
     </>
   )
 }
