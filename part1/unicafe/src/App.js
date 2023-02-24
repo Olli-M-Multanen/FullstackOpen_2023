c7510f444dbff8dc3847cd9fe4f23a747dfca2bb
Olli-M-Multanen <olli.multanen@gmail.com>
2023-02-24 10:28:53 +0100

    1.11 unicafe step6 done table version

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 const Header = () => {
   return (
-    <div>
+    <>
       <h1>Give feedback</h1>
-    </div>
+    </>
   )
 }
 
@@ -16,30 +16,37 @@ const Button = ({ handleClick, text }) => (
 
 const StatisticLine = ({text, value, sign}) => {
   return (
-    <div>
-      <p>{text} {value} {sign}</p>
-    </div>
+    <>
+    <tr>
+      <td>{text}</td>
+      <td>{value} {sign}</td>
+    </tr>
+    </>
   )
 }
 
 const Statistics = ({ good, neutral, bad, total, positive}) => {
   if (total < 1) {
     return (
-      <div>
+      <>
         <h1>statistics</h1>
         <p>No feedback given</p>
-      </div>
+      </>
     )
   } else return (
-    <div>
+    <>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={total} />
-      <StatisticLine text="average" value={positive / total} />
-      <StatisticLine text="average" value={good / total * 100} sign="%"/>
-    </div>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={positive / total} />
+          <StatisticLine text="average" value={good / total * 100} sign="%"/>
+        </tbody>
+      </table>
+    </>
   )
 }
 
@@ -51,18 +58,15 @@ const App = () => {
   const [positive, setPositive] = useState(0)
 
   const handleGood = () => {
-    console.log("Good")
     setGood(good + 1)
     setTotal(total +1)
     setPositive(positive + 1)
   }
   const handleNeutral = () => {
-    console.log("Neutral")
     setNeutral(neutral + 1)
     setTotal(total +1)
   }
   const handleBad = () => {
-    console.log("Bad")
     setBad(bad + 1)
     setTotal(total +1)
     setPositive(positive - 1)
