7e99a6c80715efa67d0c257f57dda781b29f9501
Olli-M-Multanen <olli.multanen@gmail.com>
2023-03-28 08:16:10 +0200

    3.3 backend phonebook step3 done

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -38,6 +38,18 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+
+  })
+
+
 app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has infor for ${counter} people</p> <p>${timeNow}</p>`)
   })
