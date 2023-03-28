5a8b848a771d267efda92863531eb787c6124b91
Olli-M-Multanen <olli.multanen@gmail.com>
2023-03-28 13:18:43 +0200

    3.5 backend phonebook step5 done

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -54,6 +54,38 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
   })
 
+const generateId = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+
+    const newId = Math.floor(Math.random() * (max - min) + min)
+    return newId
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        id: generateId(1, 10000),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
 
 app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has infor for ${counter} people</p> <p>${timeNow}</p>`)
